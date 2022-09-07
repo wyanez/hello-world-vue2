6e848c2887aba29d0cc7e3a1f6e4c4d42661a761
William Yanez <wyanez@gmail.com>
2022-09-07 01:09:38 -0400

    deploy

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+# abort on errors
+#set -e
+# build
+yarn run build
+# navigate into the build output directory
+cd dist
+
+#git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:wyanez/hello-world-vue2.git main:gh-pages
+cd -
